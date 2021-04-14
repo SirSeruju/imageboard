0f972a7a0bc65d51c2113303034da10e66b8dd89
Seruju <sir.seruju@yandex.ru>
2021-04-14 17:52:27 +0100

    Fix database

--- database/threads.py
+++
@@ -1,9 +0,0 @@
-import sqlalchemy
-from .db_session import SqlAlchemyBase
-
-class Threads(SqlAlchemyBase):
-    __tablename__ = 'threads'
-
-    id     = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    parent = sqlalchemy.Column(sqlalchemy.Integer)
-    body   = sqlalchemy.Column(sqlalchemy.String)
