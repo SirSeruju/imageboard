1341b1f833f9daa43bbac9a55a0b897a712d11cb
Seruju <sir.seruju@yandex.ru>
2021-04-14 01:03:49 +0100

    Delete database/messages.py

--- database/messages.py
+++
@@ -1,8 +0,0 @@
-import sqlalchemy
-from .db_session import SqlAlchemyBase
-
-class User(SqlAlchemyBase):
-	__tablename__ = 'messages'
-
-	id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-	speciality = sqlalchemy.Column(sqlalchemy.String)
