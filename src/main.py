38cc81a6fbcc80fac39c3ab6c2905f569a0f5504
Seruju <sir.seruju@yandex.ru>
2021-04-16 21:31:22 +0300

    Add route to thread in general

--- src/main.py
+++ src/main.py
@@ -1,13 +1,24 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify, render_template
 from database.db_session import *
 from flask_restful import Api
 from api import ThreadResource, ThreadListResource
+from database.thread import Thread
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(ThreadResource, '/api/threads/<int:thread_id>')
 api.add_resource(ThreadListResource, '/api/threads')
+
+@app.route("/<int:thread_id>")
+def getThread(thread_id):
+    db_sess = create_session()
+    parent = db_sess.query(Thread).get(thread_id)
+    if not parent:
+        return make_response(jsonify({"error": "Not found"}), 404)
+    threads = db_sess.query(Thread).filter(Thread.parent == thread_id).all()
+    print(len(threads))
+    return render_template("index.html", parent=parent, threads=threads)
 
 def main():
     global_init("threads.db")
